4d5ec9186a0bf8265a11495dd766f4578af00372
Youuu <31610705+73-yy@users.noreply.github.com>
2022-04-10 17:31:33 +0900

    complete kaizen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./styles.css";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
 
 export const App = () => {
   // TODOリストは状態が変化していく＝Reactでは状態をstateで管理する。
@@ -27,7 +30,6 @@ export const App = () => {
     setCompleteTodos(newCompleteTodos);
   };
   const onClickDelete = (index) => {
-    // console.log(index);
     const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
@@ -42,56 +44,21 @@ export const App = () => {
   };
   return (
     <>
-      {/* jsxではclassはclassNameで指定する。 */}
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-          {/* <div className="list-row">
-            <li>aaaa</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div> */}
-          {/* <div className="list-row">
-            <li>aaaa</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div> */}
-        </ul>
-      </div>
-      <div className="complete-area">
-        <p className="title">完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            );
-          })}
-          {/* <div className="list-row">
-            <li>aaaa</li>
-            <button>戻す</button>
-          </div> */}
-        </ul>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5 && true}
+      />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるTodoは5個まで</p>
+      )}
+      <IncompleteTodos
+        incompleteTodos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos completeTodos={completeTodos} onClick={onClickBack} />
     </>
   );
 };
